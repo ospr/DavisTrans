34af6ba36a600e1181c1fc3e5433c68a171f5119
Kip <ospr@users.noreply.github.com>
2009-11-04 01:59:07 +0000

    - Added DatabaseManager singleton class to manage processing of GoogleTransitFeed data and managing the core data store. - Added TestDB.sqlite file to bundle to use as a temporary core data store.

---
+++ Unitrans/Classes/Other/DatabaseManager.h
@@ -0,0 +1,64 @@
+//
+//  DatabaseManager.h
+//  Unitrans
+//
+//  Created by Kip Nicol on 10/28/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+typedef enum _ProcessStep {
+    kProcessAgencyStep = 0, 
+    kProcessRouteStep = 1,
+    kProcessShapeStep = 2, 
+    kProcessStopStep = 3,       
+    kProcessCalendarStep = 4,    
+    kProcessCalendarDateStep = 5,
+    kProcessTripStep = 6,
+    kProcessStopTimeStep = 7
+} ProcessStep;
+
+@class Agency;
+
+@interface DatabaseManager : NSObject {
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+    
+    NSMutableDictionary *agencies;
+    NSMutableDictionary *calendars;
+    NSMutableDictionary *routes;
+    NSMutableDictionary *shapes;
+    NSMutableDictionary *stops;
+    NSMutableDictionary *trips;
+    
+    ProcessStep processStep;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
++ (DatabaseManager *)sharedDatabaseManager;
+
+- (Agency *)retrieveUnitransAgency:(NSError **)error;
+
+- (BOOL)createDatabaseFromGoogleTransitFeed:(NSString *)feedDirectory;
+- (id)insertNewObjectForEntityForName:(NSString *)entityName;
+- (BOOL)addEntity:(NSString *)entityString withValues:(NSArray *)values headers:(NSArray *)headers error:(NSError **)error;
+- (BOOL)setEntity:(id)entity propertyWithValue:(NSString *)value header:(NSString *)header;
+
+- (BOOL)processFile:(NSString *)path error:(NSError **)error;
+- (BOOL)processValues:(NSArray *)values withHeaders:(NSArray *)headers error:(NSError **)error;
+
+- (NSNumber *)processDoubleNumberString:(NSString *)value;
+- (NSNumber *)processUnsignedIntegerNumberString:(NSString *)value;
+- (NSNumber *)processShortNumberString:(NSString *)value;
+- (NSNumber *)processBoolNumberString:(NSString *)value;
+- (NSDate *)processDateString:(NSString *)value;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
---
+++ Unitrans/Classes/Other/DatabaseManager.m
@@ -0,0 +1,493 @@
+//
+//  DatabaseManager.m
+//  Unitrans
+//
+//  Created by Kip Nicol on 10/28/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "DatabaseManager.h"
+#import "Agency.h"
+#import "Calendar.h"
+#import "CalendarDate.h"
+#import "Route.h"
+#import "Shape.h"
+#import "Stop.h"
+#import "StopTime.h"
+#import "Trip.h"
+
+@implementation DatabaseManager
+
+#pragma mark -
+#pragma mark Singleton Methods
+
+static DatabaseManager *sharedDatabaseManager = nil;
+
++ (DatabaseManager *)sharedDatabaseManager
+{
+    if (sharedDatabaseManager == nil) {
+        sharedDatabaseManager = [[super allocWithZone:NULL] init];
+    }
+    return sharedDatabaseManager;
+}
+
++ (id)allocWithZone:(NSZone *)zone
+{
+    return [[self sharedDatabaseManager] retain];
+}
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    return self;
+}
+
+- (id)retain
+{
+    return self;
+}
+
+- (NSUInteger)retainCount
+{
+    return NSUIntegerMax;  //denotes an object that cannot be released
+}
+
+- (void)release
+{
+    //do nothing
+}
+
+- (id)autorelease
+{
+    return self;
+}
+
+#pragma mark -
+#pragma mark Memory management
+
+- (void)dealloc 
+{	
+    [managedObjectContext release];
+    [managedObjectModel release];
+    [persistentStoreCoordinator release];
+    
+	[super dealloc];
+}
+
+#pragma mark -
+#pragma mark Retrieval Methods
+
+- (Agency *)retrieveUnitransAgency:(NSError **)error
+{   
+    // Create request
+    NSFetchRequest *request = [[[NSFetchRequest alloc] init] autorelease];
+    
+    // Set Route entity
+    [request setEntity:[NSEntityDescription entityForName:@"Agency" inManagedObjectContext:[self managedObjectContext]]];
+    
+    // Filter out only the unitrans agency
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name == %@", @"Unitrans (Davis)"];
+    [request setPredicate:predicate];
+    
+    // Execute fetch
+    NSArray *agencyArray = [[self managedObjectContext] executeFetchRequest:request error:error];
+    if (!agencyArray)
+        return nil;
+    
+    // Return Unitrans agency
+    return [agencyArray lastObject];
+}
+
+- (BOOL)createDatabaseFromGoogleTransitFeed:(NSString *)feedDirectory
+{
+    NSError *error;
+    
+    // Prepare GTF file paths
+    NSString *agencyFile = [feedDirectory stringByAppendingPathComponent:@"agency.txt"];
+    NSString *calendarFile = [feedDirectory stringByAppendingPathComponent:@"calendar.txt"];
+    NSString *calendarDateFile = [feedDirectory stringByAppendingPathComponent:@"calendar_dates.txt"];
+    NSString *routeFile = [feedDirectory stringByAppendingPathComponent:@"routes.txt"];
+    NSString *shapeFile = [feedDirectory stringByAppendingPathComponent:@"shapes.txt"];
+    NSString *stopsFile = [feedDirectory stringByAppendingPathComponent:@"stops.txt"];
+    NSString *stopTimesFile = [feedDirectory stringByAppendingPathComponent:@"stop_times.txt"];
+    NSString *tripsFile = [feedDirectory stringByAppendingPathComponent:@"trips.txt"];
+    
+    // Create ordered array for processing
+    NSArray *files = [NSArray arrayWithObjects:agencyFile, routeFile, shapeFile, stopsFile, calendarFile, calendarDateFile, tripsFile, stopTimesFile, nil];
+    
+    // Initialize dictionaries
+    agencies = [[NSMutableDictionary alloc] init];
+    calendars = [[NSMutableDictionary alloc] init];
+    routes = [[NSMutableDictionary alloc] init];
+    shapes = [[NSMutableDictionary alloc] init];
+    stops = [[NSMutableDictionary alloc] init];
+    trips = [[NSMutableDictionary alloc] init];
+
+    // Process files
+    for (int i = 0; i < [files count]; i += 1) {
+        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+        processStep = i;
+        if (![self processFile:[files objectAtIndex:i] error:&error])
+            return NO;
+        [pool drain];
+    }
+    
+    // Clean up dictionaries
+    [agencies release];
+    [calendars release];
+    [routes release];
+    [shapes release];
+    [stops release];
+    [trips release];
+    
+    // Save core data file
+    if (![[self managedObjectContext] save:&error]) {
+        NSLog(@"Error when trying to save core data file: %@", error);
+        return NO;
+    }
+    
+    return YES;
+}
+
+- (BOOL)processFile:(NSString *)path error:(NSError **)error
+{
+    //NSLog(@"========================= PROCESSING FILE: %@ ==========================", path);
+    
+    // Read in all text, split by newline, and extract header line
+    NSString *text = [[NSString alloc] initWithContentsOfFile:path encoding:NSUTF8StringEncoding error:error];
+    NSArray *lines = [text componentsSeparatedByString:@"\r\n"];
+    NSArray *headers = [[lines objectAtIndex:0] componentsSeparatedByString:@","];
+    
+    [text release];
+    
+    // Loop through value lines and process them
+    for (int i = 1; i < [lines count]; i += 1)
+    {
+        NSString *valuesString = [lines objectAtIndex:i];
+        if ([valuesString isEqualToString:@""])
+            continue;
+        
+        NSArray *values = [valuesString componentsSeparatedByString:@","];
+        
+        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+        BOOL ret = [self processValues:values withHeaders:headers error:error];
+        [pool drain];
+
+        if (!ret) return NO;
+    }
+    
+    return YES;
+}
+
+- (BOOL)processValues:(NSArray *)values withHeaders:(NSArray *)headers error:(NSError **)error
+{
+    switch (processStep) {
+        case kProcessAgencyStep:       return [self addEntity:@"Agency" withValues:values headers:headers error:error]; 
+        case kProcessCalendarStep:     return [self addEntity:@"Calendar" withValues:values headers:headers error:error];
+        case kProcessCalendarDateStep: return [self addEntity:@"CalendarDate" withValues:values headers:headers error:error];
+        case kProcessRouteStep:        return [self addEntity:@"Route" withValues:values headers:headers error:error];
+        case kProcessShapeStep:        return [self addEntity:@"Shape" withValues:values headers:headers error:error];
+        case kProcessStopStep:         return [self addEntity:@"Stop" withValues:values headers:headers error:error];
+        case kProcessStopTimeStep:     return [self addEntity:@"StopTime" withValues:values headers:headers error:error];
+        case kProcessTripStep:         return [self addEntity:@"Trip" withValues:values headers:headers error:error];
+            
+        // TODO: create error
+        default:
+            NSLog(@"Error: unknown process step %d", processStep);
+            return NO;
+    }
+}
+
+- (BOOL)addEntity:(NSString *)entityString withValues:(NSArray *)values headers:(NSArray *)headers error:(NSError **)error
+{
+    NSUInteger valuesCount  = [values count];
+    NSUInteger headersCount = [headers count];
+    
+    // TODO: create error here
+    if (valuesCount != headersCount) {
+        NSLog(@"Error: header count (%d) doesn not match value count (%d)", headersCount, valuesCount);
+        NSLog(@"Headers:\n%@\n\nValues:\n%@\n", headers, values);
+        return NO;
+    }
+    
+    // Create new entity
+    id entity = [self insertNewObjectForEntityForName:entityString];
+    
+    // Set entity's properties based on header using value
+    for (int i = 0; i < headersCount; i += 1) {
+        NSString *header = [headers objectAtIndex:i];
+        NSString *value  = [values objectAtIndex:i];
+        
+        // Strip away any leading/trailing whitespace (shapes.txt)
+        value = [value stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+        
+        [self setEntity:entity propertyWithValue:value header:header];
+    }
+    
+    //NSLog(@"New %@: %@", entityString, entity);
+        
+    return YES;
+}
+
+- (BOOL)setEntity:(id)entity propertyWithValue:(NSString *)value header:(NSString *)header
+{
+    if (processStep == kProcessAgencyStep)
+    {
+        Agency *agency = entity;
+        if ([header isEqualToString:@"agency_id"])
+            [agencies setObject:agency forKey:value];
+        else if ([header isEqualToString:@"agency_name"])
+            [agency setName:value];
+        else if ([header isEqualToString:@"agency_url"])
+            [agency setUrl:value];
+        else if ([header isEqualToString:@"agency_phone"])
+            [agency setPhone:value];
+    }
+    else if (processStep == kProcessCalendarStep)
+    {
+        Calendar *calendar = entity;
+        if ([header isEqualToString:@"service_id"])
+            [calendars setObject:calendar forKey:value];
+        else if ([header isEqualToString:@"sunday"])
+            [calendar setSunday:[self processBoolNumberString:value]];
+        else if ([header isEqualToString:@"monday"])
+            [calendar setMonday:[self processBoolNumberString:value]];
+        else if ([header isEqualToString:@"tuesday"])
+            [calendar setTuesday:[self processBoolNumberString:value]];
+        else if ([header isEqualToString:@"wednesday"])
+            [calendar setWednesday:[self processBoolNumberString:value]];
+        else if ([header isEqualToString:@"thursday"])
+            [calendar setThursday:[self processBoolNumberString:value]];
+        else if ([header isEqualToString:@"friday"])
+            [calendar setFriday:[self processBoolNumberString:value]];
+        else if ([header isEqualToString:@"saturday"])
+            [calendar setSaturday:[self processBoolNumberString:value]];
+        else if ([header isEqualToString:@"start_date"])
+            [calendar setStartDate:[self processDateString:value]];
+        else if ([header isEqualToString:@"end_date"])
+            [calendar setEndDate:[self processDateString:value]];
+    }
+    else if (processStep == kProcessCalendarDateStep)
+    {
+        CalendarDate *calendarDate = entity;
+        if ([header isEqualToString:@"service_id"])
+            [calendarDate setCalendar:[calendars objectForKey:value]];
+        else if ([header isEqualToString:@"date"])
+            [calendarDate setDate:[self processDateString:value]];
+        else if ([header isEqualToString:@"exception_type"])
+            [calendarDate setExceptionType:[self processShortNumberString:value]];
+    }
+    else if (processStep == kProcessRouteStep) 
+    {
+        Route *route = entity;
+        if ([header isEqualToString:@"route_id"])
+            [routes setObject:route forKey:value];
+        else if ([header isEqualToString:@"agency_id"])
+            [route setAgency:[agencies objectForKey:value]];
+        else if ([header isEqualToString:@"route_short_name"])
+            [route setShortName:value];
+        else if ([header isEqualToString:@"route_long_name"])
+            [route setLongName:value];
+        else if ([header isEqualToString:@"route_type"])
+            [route setType:[self processShortNumberString:value]];
+        else if ([header isEqualToString:@"route_color"])
+            [route setColor:[self processUnsignedIntegerNumberString:value]];
+        else if ([header isEqualToString:@"route_text_color"])
+            [route setTextColor:[self processUnsignedIntegerNumberString:value]];
+    }
+    else if (processStep == kProcessShapeStep)
+    {
+        Shape *shape = entity;
+        if ([header isEqualToString:@"shape_id"]) {
+            // Create new shape set if shape_id is not found
+            NSMutableSet *shapeSet = [shapes objectForKey:value];
+            if (!shapeSet) {
+                shapeSet = [NSMutableSet set];
+                [shapes setObject:shapeSet forKey:value];
+            }
+            [shapeSet addObject:shape];
+        }
+        else if ([header isEqualToString:@"shape_pt_lat"])
+            [shape setPointLatitude:[self processDoubleNumberString:value]];
+        else if ([header isEqualToString:@"shape_pt_lon"])
+            [shape setPointLongitude:[self processDoubleNumberString:value]];
+        else if ([header isEqualToString:@"shape_pt_sequence"])
+            [shape setPointSequence:[self processUnsignedIntegerNumberString:value]];
+    }
+    else if (processStep == kProcessStopStep)
+    {
+        Stop *stop = entity;
+        if ([header isEqualToString:@"stop_id"])
+            [stops setObject:stop forKey:value];
+        else if ([header isEqualToString:@"stop_name"])
+            [stop setName:value];
+        else if ([header isEqualToString:@"stop_desc"])
+            [stop setStopDescription:value];
+        else if ([header isEqualToString:@"stop_lat"])
+            [stop setLatitude:[self processDoubleNumberString:value]];
+        else if ([header isEqualToString:@"stop_lon"])
+            [stop setLongitude:[self processDoubleNumberString:value]];
+        else if ([header isEqualToString:@"stop_code"])
+            [stop setCode:[self processUnsignedIntegerNumberString:value]];
+    }
+    else if (processStep == kProcessStopTimeStep)
+    {        
+        StopTime *stopTime = entity;
+        if ([header isEqualToString:@"trip_id"])
+            [stopTime setTrip:[trips objectForKey:value]];
+        else if ([header isEqualToString:@"arrival_time"])
+            [stopTime setArrivalTimeFromTimeString:value];
+        else if ([header isEqualToString:@"departure_time"])
+            [stopTime setDepartureTimeFromTimeString:value];
+        else if ([header isEqualToString:@"stop_id"])
+            [stopTime setStop:[stops objectForKey:value]];
+        else if ([header isEqualToString:@"stop_sequence"])
+            [stopTime setSequence:[self processShortNumberString:value]];
+        else if ([header isEqualToString:@"pickup_type"])
+            [stopTime setPickupType:[self processShortNumberString:value]];
+        else if ([header isEqualToString:@"drop_off_type"])
+            [stopTime setDropOffType:[self processShortNumberString:value]];        
+    }
+    else if (processStep == kProcessTripStep)
+    {
+        Trip *trip = entity;
+        if ([header isEqualToString:@"route_id"])
+            [trip setRoute:[routes objectForKey:value]];
+        else if ([header isEqualToString:@"service_id"])
+            [trip setCalendar:[calendars objectForKey:value]];
+        else if ([header isEqualToString:@"trip_id"])
+            [trips setObject:trip forKey:value];
+        else if ([header isEqualToString:@"trip_headsign"])
+            [trip setHeadsign:value];
+        else if ([header isEqualToString:@"shape_id"])
+            [trip setShapes:[shapes objectForKey:value]];
+        else if ([header isEqualToString:@"direction_id"])
+            [trip setDirection:[self processShortNumberString:value]];
+        else if ([header isEqualToString:@"block_id"])
+            [trip setBlock:[self processShortNumberString:value]];
+    }
+    
+    return YES;
+}
+
+- (NSNumber *)processDoubleNumberString:(NSString *)value
+{   
+    return [NSNumber numberWithDouble:[value doubleValue]];
+}
+                                     
+- (NSNumber *)processUnsignedIntegerNumberString:(NSString *)value
+{
+    return [NSNumber numberWithUnsignedInteger:[value intValue]];
+}
+
+- (NSNumber *)processShortNumberString:(NSString *)value
+{
+    return [NSNumber numberWithShort:[value intValue]];
+}
+             
+- (NSNumber *)processBoolNumberString:(NSString *)value
+{
+    return [NSNumber numberWithBool:[value boolValue]];
+}
+
+- (NSDate *)processDateString:(NSString *)value
+{
+    static NSDateFormatter *dateFormatter = nil;
+    
+    if (!dateFormatter) {
+        dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setDateFormat:@"yyyyMMdd"];
+    }
+    
+    return [dateFormatter dateFromString:value];
+}
+
+#pragma mark -
+#pragma mark Core Data stack
+
+/**
+ Returns the managed object context for the application.
+ If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
+ */
+- (NSManagedObjectContext *)managedObjectContext 
+{	
+    if (managedObjectContext != nil)
+        return managedObjectContext;
+	
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (coordinator != nil) {
+        managedObjectContext = [[NSManagedObjectContext alloc] init];
+        [managedObjectContext setPersistentStoreCoordinator:coordinator];
+    }
+    return managedObjectContext;
+}
+
+/**
+ Returns the managed object model for the application.
+ If the model doesn't already exist, it is created by merging all of the models found in the application bundle.
+ */
+- (NSManagedObjectModel *)managedObjectModel 
+{	
+    if (managedObjectModel != nil)
+        return managedObjectModel;
+
+    managedObjectModel = [[NSManagedObjectModel mergedModelFromBundles:nil] retain];    
+    return managedObjectModel;
+}
+
+/**
+ Returns the persistent store coordinator for the application.
+ If the coordinator doesn't already exist, it is created and the application's store added to it.
+ */
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator 
+{	
+    if (persistentStoreCoordinator != nil)
+        return persistentStoreCoordinator;
+	
+    //NSURL *storeUrl = [NSURL fileURLWithPath:[[self applicationDocumentsDirectory] stringByAppendingPathComponent:@"TestDB.sqlite"]];
+	NSURL *storeUrl = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"TestDB" ofType:@"sqlite"]];
+    
+    NSLog(@"store url = %@", storeUrl);
+    
+    // TODO: remove this/modify before production
+    //NSError *rmError;
+    //if (![[NSFileManager defaultManager] removeItemAtPath:[storeUrl path] error:&rmError])
+     //   NSLog(@"Error removing store '%@': %@", [storeUrl path], rmError);
+            
+	NSError *error = nil;
+    persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+    if (![persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeUrl options:nil error:&error]) {
+		/*
+		 Replace this implementation with code to handle the error appropriately.
+		 
+		 abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. If it is not possible to recover from the error, display an alert panel that instructs the user to quit the application by pressing the Home button.
+		 
+		 Typical reasons for an error here include:
+		 * The persistent store is not accessible
+		 * The schema for the persistent store is incompatible with current managed object model
+		 Check the error message to determine what the actual problem was.
+		 */
+		NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+		abort();
+    }    
+	
+    return persistentStoreCoordinator;
+}
+
+- (id)insertNewObjectForEntityForName:(NSString *)entityName
+{
+    return [NSEntityDescription insertNewObjectForEntityForName:entityName inManagedObjectContext:[self managedObjectContext]];
+}
+
+#pragma mark -
+#pragma mark Application's Documents directory
+
+/**
+ Returns the path to the application's Documents directory.
+ */
+- (NSString *)applicationDocumentsDirectory 
+{
+	return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+}
+
+
+@end
