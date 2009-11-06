a8ee1ff24d1b0fd37789dd09c10ba4db541f2b2d
kzheng <kzheng@d66ba40b-609a-48b0-b94a-bfaac553b4ac>
2009-11-06 02:44:01 +0000

    Barebones StopTimeViewController

---
+++ Unitrans/Classes/Controllers/StopTimeViewController.h
@@ -0,0 +1,18 @@
+//
+//  StopTimeViewController.h
+//  Unitrans
+//
+//  Created by Ken Zheng on 11/5/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface StopTimeViewController : UITableViewController {
+	NSArray *arrivalTimes;
+}
+
+@property (nonatomic, retain) NSArray *arrivalTimes;
+
+@end
