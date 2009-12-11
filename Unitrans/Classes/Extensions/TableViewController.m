c526ff1c057ef8603d22115e03e098d8ec49b3df
Kip <ospr@users.noreply.github.com>
2009-12-11 20:01:55 +0000

    - TableViewController class now correctly reloads tableview only in viewdidLoad method. - Deselecting of cells is now correctly animated in TableViewController. - Fixed memory leak in TableViewController where the tableView was not getting released (oops!).

--- Unitrans/Classes/Extensions/TableViewController.m
+++ Unitrans/Classes/Extensions/TableViewController.m
@@ -13,45 +13,25 @@
 
 @dynamic tableView;
 
-/*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
-}
-*/
-
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
-    [super viewDidLoad];
-}
-*/
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
-
-- (void)viewWillAppear:(BOOL)animated
+- (void)dealloc
 {
+    [tableView release];
+    
+    [super dealloc];
+}
+
+- (void)viewDidLoad 
+{
+    [super viewDidLoad];
+    
     [tableView reloadData];
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
-    [tableView deselectRowAtIndexPath:[tableView indexPathForSelectedRow] animated:YES];
+    [super viewDidAppear:animated];
+    
+    [tableView deselectRowAtIndexPath:[tableView indexPathForSelectedRow] animated:animated];
     [tableView flashScrollIndicators];
 }
 
@@ -65,11 +45,6 @@
 - (void)viewDidUnload {
 	// Release any retained subviews of the main view.
 	// e.g. self.myOutlet = nil;
-}
-
-
-- (void)dealloc {
-    [super dealloc];
 }
 
 - (void)setTableView:(UITableView *)tv
