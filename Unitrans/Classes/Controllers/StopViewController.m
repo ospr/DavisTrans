b7d204e4d3e676df5e395dfe42726e4b6c2ad177
Kip <ospr@users.noreply.github.com>
2009-12-11 20:04:00 +0000

    - Moved beginContinuousPredictionsUpdates to viewdidAppear so that the stopTime cells are animated when deselected (previously the tableView was being reloaded which deselected the cell without animation)

--- Unitrans/Classes/Controllers/StopViewController.m
+++ Unitrans/Classes/Controllers/StopViewController.m
@@ -90,9 +90,9 @@
     [self addUpdateNextStopTimeTimer];
 }
 
-- (void)viewWillAppear:(BOOL)animated
+- (void)viewDidAppear:(BOOL)animated
 {
-    [super viewWillAppear:animated];
+    [super viewDidAppear:animated];
     
     [self beginContinuousPredictionsUpdates];
 }
